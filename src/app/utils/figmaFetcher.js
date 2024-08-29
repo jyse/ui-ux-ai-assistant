721d23ba88d6fdc7550f69ee6677ac407b1d3a6f
Lance Toledo <50975456+lancetoledo@users.noreply.github.com>
2024-08-29 17:02:31 -0400

    UPDATE:figmaFetcher

--- src/app/utils/figmaFetcher.js
+++ src/app/utils/figmaFetcher.js
@@ -97,21 +97,16 @@ async function processUIKit(fileId, outputDir) {
 }
 
 const fileKeys = [
-    'FPGFvyaS4qAO9UDYpOXBcx', // Material 3 Design Kit
-    'O3b4NkQnCBw3ElTjCdZ3Qj', // iOS 17 UI Kit
-    'sEhsAZcx41HgTPQ5gWoq63', // Tailwind CSS UI Kit
-    'zj01ZZSXvHaPCIk2UbWkd7', // Ant Design 5.0
-    'W1qljjfxjPnVRjb4gxjKAD', // Figma for Developers
+    'r0rkXHzWhWQF4fw951UUTM', // Untitled UI – FREE Figma UI kit and design system v2.0
+    '1nmPr3ZbIxiwyyYMiejyqS', // iOS 16 UI Kit for Figma
+    'NEsdoTg6cmhsBO8SZ1LPcj', // Mobile UI kit
+    'TeEDVA3atoMihlKUoydcae', // Instagram UI Kit 1.0
+    'eWBIN3iwIApgNcCq3MpWu0', // Essential UI - Figma UI Kit
+    'enUK2QmBWn8XIlqhI70Fqs', // Microsoft Teams UI Kit
+    'jUUax5RKeO1S7Zf0CttAjy', // Wireframes Kit - Free wireframing Websites and SaaS UI/UX
+    '3Ubj0IUKgG8z5AopZJzrZD', // Mobile App UI Kit
   ];
   
-  https://www.figma.com/design/r0rkXHzWhWQF4fw951UUTM/%E2%9D%96-Untitled-UI-%E2%80%93-FREE-Figma-UI-kit-and-design-system-v2.0-(Community)?t=3TlM5aqgnrGInKaU-0
-  https://www.figma.com/design/1nmPr3ZbIxiwyyYMiejyqS/iOS-16-UI-Kit-for-Figma-(Community)?node-id=1002-6668&node-type=CANVAS&t=BGvEShYw7Wrsre1H-0
-  https://www.figma.com/design/NEsdoTg6cmhsBO8SZ1LPcj/Mobile-UI-kit-(Community)?node-id=194-1561&node-type=CANVAS&t=GnQia22c8QEPnXae-0
-  https://www.figma.com/design/TeEDVA3atoMihlKUoydcae/Instagram-UI-Kit-1.0-(Community)?t=280o4UVdqXOo4QrJ-0
-  https://www.figma.com/design/eWBIN3iwIApgNcCq3MpWu0/Essential-UI---Figma-Ui-Kit-(Community)?node-id=69-5241&node-type=CANVAS&t=ZEVuqnRCDfoWhO0t-0
-  https://www.figma.com/design/enUK2QmBWn8XIlqhI70Fqs/Microsoft-Teams-UI-Kit-(Community)?t=65rrH3mnOv7ehveN-0
-  https://www.figma.com/design/jUUax5RKeO1S7Zf0CttAjy/Wireframes-Kit---Free-wireframing-Websites-and-SaaS-UI-UX-(Community)?node-id=292-11637&node-type=CANVAS&t=dWsEN4cxBh0y3ORF-0
-  https://www.figma.com/design/3Ubj0IUKgG8z5AopZJzrZD/Mobile-App-Ui-Kit-(Community)-(Copy)?node-id=0-1&node-type=CANVAS&t=l0Ao3uEbJwBMOiLP-0
   const outputDir = './figma_components';
   
   if (!fs.existsSync(outputDir)){
