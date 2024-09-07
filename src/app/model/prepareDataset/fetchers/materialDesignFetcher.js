f2a0ee205e2aa04154261cbdec58618f0403d5a2
jyse <jessythe@gmail.com>
2024-09-07 22:35:47 +0200

    before i change materialDesignFetcher.js with more filters and this time regarding the images folders in the material Design Components because there are only .png etc images we need for the dataset in these images folders

--- src/app/model/prepareDataset/fetchers/materialDesignFetcher.js
+++ src/app/model/prepareDataset/fetchers/materialDesignFetcher.js
@@ -2,20 +2,24 @@ import fs from "fs";
 import { fileURLToPath } from "url";
 import path from "path";
 
-// Equivalent to __filename and __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const repoDir = path.resolve(
   __dirname,
   "../../../../../material-components-web/packages"
-); // The correct relative path to the repo
+);
 const dataDir = path.resolve(
   __dirname,
   "../../../../../data/raw/materialdesign"
-); // Moving data outside of fetchers
+);
 
-// Function to copy files and directories recursively
+// Function to filter only image files
+function isImageFile(fileName) {
+  return /\.(png|jpe?g)$/i.test(fileName);
+}
+
+// Function to copy files and directories recursively, but only images
 function copyRecursiveSync(src, dest) {
   if (!fs.existsSync(dest)) {
     fs.mkdirSync(dest, { recursive: true });
@@ -29,21 +33,20 @@ function copyRecursiveSync(src, dest) {
     const stats = fs.statSync(srcPath);
 
     if (stats.isDirectory()) {
-      // If it's a directory, copy it recursively
       copyRecursiveSync(srcPath, destPath);
-    } else {
-      // If it's a file, copy it
+    } else if (isImageFile(item)) {
       fs.copyFileSync(srcPath, destPath);
-      console.log(`Copied file: ${srcPath} -> ${destPath}`);
+      console.log(`Copied image: ${srcPath} -> ${destPath}`);
     }
   });
 }
 
-// Main function to initiate the copying process
 function main() {
   try {
     copyRecursiveSync(repoDir, dataDir);
-    console.log(`All components from 'packages' copied to: ${dataDir}`);
+    console.log(
+      `Filtered image components from 'packages' copied to: ${dataDir}`
+    );
   } catch (error) {
     console.error("Error copying components:", error);
   }
